a24bd49065b0627698c8984d6baab8ba7eba316e
mjkwon <kmj7475@gmail.com>
2021-02-09 18:56:16 +0900

    remove file

--- MIPROJECT_newTeam/src/main/java/com/etoile/app/admin/mapper/AdminInfoMapper.java
+++
@@ -1,8 +0,0 @@
-package com.etoile.app.admin.mapper;
-
-import com.etoile.app.vo.AdminInfoVO;
-
-public interface AdminInfoMapper {
-	//로그인
-	public int adminLogin(AdminInfoVO vo);
-}
--- MIPROJECT_newTeam/src/main/java/com/etoile/app/vo/AdminInfoVO.java
+++
@@ -1,9 +0,0 @@
-package com.etoile.app.vo;
-
-import lombok.Data;
-
-@Data
-public class AdminInfoVO {
-	private String id;
-	private String password;
-}
